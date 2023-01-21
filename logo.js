a6e40ef1dee8fed17556dfff60e2f250147e183a
noyoto <48520106+noyoto@users.noreply.github.com>
2023-01-21 17:13:19 +0100

    also vertical 2.0

--- logo.js
+++ logo.js
@@ -1,20 +1,13 @@
 var text = document.querySelector(".text-3xl");
 
 setInterval(function(){
-    // Randomize the text's color
     text.style.color = getRandomColor();
-    // Randomize the text's horizontal position and add a blur effect
-    text.style.transform = "translate(" + Math.random() * 20 + "px, 0px)";
-     text.style.transform = "translate(0px, " + Math.random() * 20 + "px)";
-}, 100);
+    // Randomize the text's horizontal and vertical position
+    text.style.transform = "translate(" + Math.random() * 20 + "px, " + Math.random() * 20 + "px)";
     text.style.filter = "blur(" + Math.random() * 5 + "px)";
 }, 100);
 
 function getRandomColor() {
   var letters = "0123456789ABCDEF";
   var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+  for (var i = 0; i < 6; i
